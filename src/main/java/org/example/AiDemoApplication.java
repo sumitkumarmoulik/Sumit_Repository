50c5a476e738dd34c9d5dcc58efb21ff9c9837eb
Sumit_Repository <moulik.sumit@gmail.com>
2025-12-03 19:29:11 +0530

    Project structure updated by copilot

--- src/main/java/org/example/AiDemoApplication.java
+++ src/main/java/org/example/AiDemoApplication.java
@@ -3,7 +3,7 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Spring boot Rest API application for github copilot demo
 @SpringBootApplication
 public class AiDemoApplication {
     public static void main(String[] args) {
